ecc370a1337d758731254dbb95a622b318f46758
gilbertomourao <gilbertojos.mourao@gmail.com>
2021-02-06 23:07:39 -0300

    novo arquivo de teste

---
+++ testbench/tb_array.c
@@ -0,0 +1,24 @@
+/**
+ * Autor: Gilberto José Guimarães de Sousa Mourão
+ *
+ * Testbench das funções relacionadas a arrays em LINP
+ */
+
+#include <linp.h>
+
+int main()
+{
+	Linp_Mat *mat1 = lp.criarmat(100,100);
+	Linp_Mat *mat2 = lp.criarmat(100,100);
+
+	lp.lerarquivo(mat1, "txt/camomile.txt");
+	lp.lerarquivo(mat2, "txt/matriz_P1.txt");
+
+	lp.dispmat(mat1, "Matriz 1");
+	lp.dispmat(mat2, "Matriz 2");
+
+	lp.destruirmat(mat1);
+	lp.destruirmat(mat2);
+
+	return 0;
+}
